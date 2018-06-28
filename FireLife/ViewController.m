1c1a1bb1e00216a95465fa8dff715bb24c5ec84c
wangchenglong <wangchenglong@sdjictec.com>
2018-06-28 15:24:29 +0800

    Initial Commit

---
+++ FireLife/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FireLife
+//
+//  Created by 王成龙 on 2018/6/28.
+//  Copyright © 2018年 charlie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
